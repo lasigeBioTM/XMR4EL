ffb255373dba9193d5fb5108f170345ecd23ca4c
Joao <fc56311@alunos.fc.ul.pt>
2025-02-03 16:25:12 +0000

    Twecking the hierarchical clustering

--- src/machine_learning/hierarchical_linear_model.py
+++ src/machine_learning/hierarchical_linear_model.py
@@ -71,6 +71,8 @@ class HieararchicalLinearModel():
                 
                 return times_list
             
+            print(Y)
+            
             # Splitting Data
             X_train, X_test, y_train, y_test = train_test_split(
             X, # Embeddings
