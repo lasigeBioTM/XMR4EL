f71de15a5bb84170e391f1dd5edf63f2803fdc6c
Joao <fc56311@alunos.fc.ul.pt>
2025-02-05 15:07:59 +0000

    Revamped save and load methods as utils so it can load the new models with gpu support

--- src/app/utils.py
+++ src/app/utils.py
@@ -1,79 +1,96 @@
+import subprocess
+import shutil
+import os
+
+from src.machine_learning.hierarchical_clustering import DivisiveHierarchicalClustering
 from src.featurization.preprocessor import Preprocessor
-from src.machine_learning.cpu.ml import AgglomerativeClusteringCPU
 from src.featurization.vectorizer import BioBertVectorizer, TfidfVectorizer
-from src.machine_learning.clustering import Clustering
 from src.machine_learning.hierarchical_linear_model import HierarchicalLinearModel
 
+
+def is_cuda_available():
+    # Default to using CPU models and set gpu_available to False
+    gpu_available = False
+
+    # Check if nvidia-smi is available on the system
+    if shutil.which('nvidia-smi'):
+        try:
+            # Run the nvidia-smi command to check for an NVIDIA GPU
+            result = subprocess.run(['nvidia-smi'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+            gpu_available = True
+        except subprocess.CalledProcessError as e:
+            # Print the error message and continue execution
+            print(f"GPU acceleration is unavailable: {e}. Defaulting to CPU models.")
+    else:
+        print("nvidia-smi command not found. Assuming no NVIDIA GPU.")
+
+    # If GPU is available, import the GPU-specific models
+    if gpu_available:
+        try:
+            
+            from src.machine_learning.gpu.ml import KMeansGPU, LogisticRegressioGPU
+            
+        except ImportError as e:
+            print(f"Error importing GPU models: {e}. Defaulting to CPU models.")
+            
+            from src.machine_learning.cpu.ml import KMeansCPU, LogisticRegressionCPU
+            
+            gpu_available = False  # If import fails, fall back to CPU
+
+    return gpu_available
+
+GPU_AVAILABLE = is_cuda_available()
 
 def load_train_and_labels_file(train_filepath, labels_filepath):
     print("Getting Processed Labels from Preprocessor")
     return Preprocessor.load_data_from_file(train_filepath, labels_filepath)
 
-def create_vectorizer(corpus):
-    vectorizer_path = "data/processed/vectorizer"
-    print("Processing Vectorizer Algorithm")
-    model = TfidfVectorizer.train(corpus)
-    model.save(vectorizer_path)
-    print("Saved Vectorizer\n")
+def create_bio_bert_vectorizer(corpus, directory):
+    print("Running BioBert")
+    assert os.path.exists(directory),f"{directory} does not exist"
+    embeddings = BioBertVectorizer.predict(corpus)
+    Preprocessor.save_biobert_labels(embeddings, directory)
+    print("Saved BioBert Embeddings")
+    return embeddings
 
-    return model
-
-def load_vectorizer(vectorizer_path):
-    print("Trying to Load the Embeddings from Preprocessor")
-    try:
-        model = Preprocessor.load(vectorizer_path)
-        print(f"Loaded Vectorizer, Type: {model.model_type}\n")
-    except Exception as e:
-        print(f"Could not load Vectorizer ({e}). Run the Training Script")
-        exit()
-
-    return model
-
-def create_hierarchical_clustering(embeddings):
-    clustering_path = "data/processed/clustering"
-    print("Processing Hierarchical Clustering Algorithm")
-    # Changing to Agglomerative Clustering
-    model = AgglomerativeClusteringCPU.train(embeddings.toarray())
-    model.save(clustering_path)
-    print("Saved Cluster Labels")
-
-    return model
-
-def load_hierarchical_clustering(clustering_path):
-    print("Trying to Load the Clustering Labels From Clustering")
-    try:
-        model = Clustering.load(clustering_path)
-        print(f"Loaded Clustering Model, Type: {model.model_type}\n")
-    except Exception as e:
-        print(f"Could not load Clustering Model ({e}). Run the Training Script")
-        exit()
-
-    return model
-
-def create_hierarchical_linear_model(X_train_feat, Y_train, k):
-    hlm_path = "data/processed/hlm"
-
-    print("Processing Hierarchical Linear Algorithm")
-    model = HierarchicalLinearModel.execute_pipeline(X_train_feat, Y_train, k)
-    model.save(hlm_path)
-    print("Hierarchical Linear Model Saved")
-
-    return model
-
-def load_hierarchical_linear_model(hlm_path):
-    try:
-        model = HierarchicalLinearModel.load(hlm_path)
-        print(f"Loaded HierarchicalLinearModel, Type: Express what clustering and linear model was used\n")
-    except Exception as e:
-        print(f"Could not load Hierarchical Linear Model ({e}). Run the Training Script")
-        exit()
+def load_bio_bert_vectorizer(directory):
+    print("Loading BioBert Labels")
+    return Preprocessor.load_biobert_labels(directory)
     
+def create_tfidf_vectorizer(corpus, directory):
+    print("Running train on TF-IDF Vectorizer")
+    assert os.path.exists(directory),f"{directory} does not exist"
+    model = TfidfVectorizer.train(corpus)
+    model.save(directory)
+    print("Saved TF-IDF Model")
     return model
 
-def run_machine_learning_matching(embeddings, clustering_labels):
-    print("Trainning")
-    # TrainCPU.train(embeddings, clustering_labels)
-    HierarchicalLinearModel.execute_pipeline(embeddings, clustering_labels)
+def load_tdidf_vectorizer(directory):
+    print("Loading TF-IDF Vectorizer")
+    assert os.path.exists(directory),f"{directory} does not exist"
+    model = Preprocessor.load(directory)
+    return model
 
-def run_ranking_model():
-    pass
+def create_hierarchical_clustering(X_train_feat):
+    if GPU_AVAILABLE:
+        print("Processing Hierarchical Clustering Algorithm with GPU SUPPORT")
+        divisive_hierarchical_clustering = DivisiveHierarchicalClustering.fit(X_train_feat, CLUSTERING_MODEL=KMeansGPU.create_model())
+    else:
+        print("Processing Hierarchical Clustering Algorithm with CPU SUPPORT")
+        divisive_hierarchical_clustering = DivisiveHierarchicalClustering.fit(X_train_feat, CLUSTERING_MODEL=KMeansCPU.create_model())
+    return divisive_hierarchical_clustering.labels
+
+def create_hierarchical_linear_model(X_train_feat, Y_train_feat, k):
+    if GPU_AVAILABLE:
+        hierarchical_linear_model = HierarchicalLinearModel.fit(
+            X_train_feat, Y_train_feat, 
+            LINEAR_MODEL=LogisticRegressionGPU.create_model(), CLUSTERING_MODEL=KMeansGPU.create_model(), top_k=3
+        )
+    else:
+        hierarchical_linear_model = HierarchicalLinearModel.fit(
+            X_train_feat, Y_train_feat, 
+            LINEAR_MODEL=LogisticRegressionCPU.create_model(), CLUSTERING_MODEL=KMeansCPU.create_model(), top_k=3
+        )
+
+    return hierarchical_linear_model.top_k, hierarchical_linear_model.top_k_score
+
