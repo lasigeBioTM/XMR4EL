44056e3e8a9e7cc76116f9b762a2f323abe56cf6
Joao <fc56311@alunos.fc.ul.pt>
2025-02-13 12:07:55 +0000

    Revamped Hierarchical Clustering

--- src/machine_learning/hierarchical_clustering.py
+++ src/machine_learning/hierarchical_clustering.py
@@ -1,13 +1,10 @@
 import os
-import numpy as np
 import pickle
-
+import numpy as np
+from collections import Counter
 from sklearn.preprocessing import normalize
 from sklearn.metrics import silhouette_score, pairwise_distances_argmin_min
 
-from collections import Counter
-
-from src.machine_learning.cpu.ml import KMeansCPU
      
 class DivisiveHierarchicalClustering():
     
@@ -39,70 +36,11 @@ class DivisiveHierarchicalClustering():
         return model
     
     @classmethod
-    def fit(cls, X, CLUSTERING_MODEL, depth=3, max_leaf_size=100, prefix="", random_state=0, spherical=True, gpu_usage=False):
+    def fit(cls, X, clustering_model_factory, depth=3, max_leaf_size=100, prefix="", random_state=0, spherical=True, gpu_usage=False):
         """
         Main method to perform divisive hierarchical clustering
         """
-        def compute_iterations(num_classes):
-            """
-            Compute the number of iterations based on the number of classes
-            """
-            return 100 + (10 * num_classes)
-            
-        def compute_branching_factor(num_samples, min_clusters=2, max_clusters=16):
-            """
-            Compute branching factor based on the number of samples
-            """
-            return min(max(num_samples // 100, min_clusters), max_clusters)
-
-        def encode_labels(labels_list):
-            """Convert string labels into numeric labels"""
-            label_to_idx = {}
-            return np.array([label_to_idx.setdefault(label, len(label_to_idx)) for label in labels_list])
-        
-        def count_label_occurrences(labels_list):
-            """
-            Count occurrences of each label in the list
-            """
-            return list(Counter(labels_list).items())
-        
-        def calculate_centroids(X, labels):
-            """
-            Calculate the centroids of the clusters based on the labels
-            """
-            return np.array([
-                X[labels == label].mean(axis=0) # Convert mean to dense array
-                for label in np.unique(labels)
-            ])  
-        
-        def merge_small_clusters(X, labels, min_cluster_size=10):
-            """
-            Merge clusters that are smaller than a given threshold by reassigning their points
-            """
-            unique_labels = np.unique(labels)
-            valid_labels = [label for label in unique_labels if np.sum(labels == label) >= min_cluster_size]
-            centroids = np.array([
-                X[labels == label].mean(axis=0)
-                for label in unique_labels
-            ])
-            
-            updated_labels = labels.copy()
-
-            for label in unique_labels:                
-                cluster_indices = np.where(labels == label)[0]
-                if len(cluster_indices) <= min_cluster_size:
-                    # Reassign points to the nearest valid centroid
-                    cluster_points = X[cluster_indices]
-                    distances = pairwise_distances_argmin_min(cluster_points, centroids)[1]
-                    closest_centroid_idx = np.argmin(distances)
-                    updated_labels[labels == label] = valid_labels[closest_centroid_idx]
-            
-            return updated_labels
-        
-        # Depth = 1, 0.005757
-        # Depth = 2, 0.008548
-        # Depth = 3, 0.011572, clearing min=10, 0.012697098340191351 127
-        # Depth = 4, 0.011774 
+    
         def recursive_clustering(X, depth=2, max_leaf_size=100, prefix="", random_state=0, spherical=True):
             """
             Recursively perform the clustering process
@@ -111,14 +49,14 @@ class DivisiveHierarchicalClustering():
             if X.shape[0] < max_leaf_size or depth == 0:
                 return {i: prefix for i in range(X.shape[0])}
             
-            n_splits = compute_branching_factor(X.shape[0])
-            n_iter = compute_iterations(n_splits)
+            n_splits = cls.__compute_branching_factor(X.shape[0])
+            n_iter = 100 + (10 * n_splits) 
             
             if spherical:
                 X = normalize(X, norm="l2", axis=1)
             
             # Fiting Clustering Model
-            kmeans_model = CLUSTERING_MODEL.create_model({'n_clusters': n_splits, 'max_iter': n_iter, 'random_state': random_state}).fit(X)
+            kmeans_model = clustering_model_factory.create_model({'n_clusters': n_splits, 'max_iter': n_iter, 'random_state': random_state}).fit(X)
                 
             cluster_labels = kmeans_model.labels_
             
@@ -145,29 +83,67 @@ class DivisiveHierarchicalClustering():
             final_labels[int(idx)] = label
         
         # Merge small clusters and update the labels
-        merged_labels = merge_small_clusters(X, np.array(final_labels), min_cluster_size=10)
+        merged_labels = cls.__merge_small_clusters(X, np.array(final_labels), min_cluster_size=10)
         
         # Calculate silhouette score
         print("Silhouette Score:", silhouette_score(X, merged_labels))
         
-        # Return the model with final labels and centroids
-        
-        labels = encode_labels(merged_labels)
-        
-        # print(labels)
-        
-        # label_occurence = count_label_occurrences(labels)
-        
-        # print(label_occurence, len(label_occurence))
+        labels = cls.__encode_labels(merged_labels)
         
         return cls(
-            clustering_model_type=CLUSTERING_MODEL.model_type, 
+            clustering_model_type=clustering_model_factory.model_type, 
             labels=labels, 
-            centroids=calculate_centroids(X, np.array(final_labels)),
+            centroids=cls.__calculate_centroids(X, np.array(final_labels)),
             gpu_usage=gpu_usage
         )
         
-    def predict(self, CLUSTERING_MODEL, test_input):
-        clustering_model = CLUSTERING_MODEL.create_model({'n_clusters': len(self.centroids), 'init': self.centroids, 'n_init': 1}).fit(test_input)
+    def predict(self, clustering_model_factory, test_input):
+        clustering_model = clustering_model_factory.create_model({'n_clusters': len(self.centroids), 'init': self.centroids, 'n_init': 1}).fit(test_input)
         return clustering_model.predict(test_input)
+    
+    @staticmethod
+    def __merge_small_clusters(X, labels, min_cluster_size=10):
+        """Merges small clusters with larger clusters."""
+        unique_labels = np.unique(labels)
+        valid_labels = [label for label in unique_labels if np.sum(labels == label) >= min_cluster_size]
+        centroids = np.array([X[labels == label].mean(axis=0) for label in unique_labels])
+            
+        updated_labels = labels.copy()
+        for label in unique_labels:                
+            cluster_indices = np.where(labels == label)[0]
+            if len(cluster_indices) <= min_cluster_size:
+                cluster_points = X[cluster_indices]
+                _, closest_centroid_idx = pairwise_distances_argmin_min(cluster_points, centroids)
+                updated_labels[labels == label] = valid_labels[closest_centroid_idx[0]]
+                
+            return updated_labels
+    
+    @staticmethod
+    def __compute_branching_factor(num_samples, min_clusters=2, max_clusters=16):
+        """
+        Compute branching factor based on the number of samples
+        """
+        return min(max(num_samples // 100, min_clusters), max_clusters)
+
+    @staticmethod
+    def __encode_labels(labels_list):
+        """Convert string labels into numeric labels"""
+        label_to_idx = {}
+        return np.array([label_to_idx.setdefault(label, len(label_to_idx)) for label in labels_list])
+    
+    @staticmethod    
+    def __count_label_occurrences(labels_list):
+        """
+        Count occurrences of each label in the list
+        """
+        return list(Counter(labels_list).items())
+        
+    @staticmethod   
+    def __calculate_centroids(X, labels):
+        """
+        Calculate the centroids of the clusters based on the labels
+        """
+        return np.array([X[labels == label].mean(axis=0) for label in np.unique(labels)])  
+        
+        
                     
