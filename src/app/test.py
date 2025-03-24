9d56d85b96151e25c4fe708d275de7357645b050
Joao <fc56311@alunos.fc.ul.pt>
2025-03-24 14:53:05 +0000

    Debugging

--- src/app/test.py
+++ src/app/test.py
@@ -12,12 +12,9 @@ else:
 source_file = "/tree/xmrtree.pkl"
 destination_folder = "/home/jvedor/test"  # Destination should be a directory
 
-# Construct the full path inside the destination folder
-destination_path = os.path.join(destination_folder, os.path.basename(source_file))
-
 # Copy the file
 if os.path.exists(source_file):
-    shutil.copy(source_file, destination_path)
-    print(f"File copied to {destination_path}")
+    shutil.copy(source_file, destination_folder)
+    print(f"File copied to {destination_folder}")
 else:
     print(f"File {source_file} does NOT exist!")
