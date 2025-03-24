1ac367b4f6a014bf784e084dc417af62b55307ce
Joao <fc56311@alunos.fc.ul.pt>
2025-03-24 11:42:25 +0000

    Debugging

--- src/featurization/transformers.py
+++ src/featurization/transformers.py
@@ -24,6 +24,7 @@ transformer_dict = {}
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Disable parallelism
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
