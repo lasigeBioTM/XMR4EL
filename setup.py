88a2b82fc714cbef446bcf1a2c858edabe094c0d
Joao <fc56311@alunos.fc.ul.pt>
2025-03-28 14:37:23 +0000

    Added a way to use pip to download model

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+def parse_requirements(filename):
+    """Read the requirements file and return a list of dependencies"""
+    with open(filename, 'r') as file:
+        return [line.strip() for line in file.readlines() if line.strip() and not line.startswith('#')]
+
+setup(
+    name='xmr4el',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
+    description='eXtreme Multi-Label Ranking for Entity Linking',
+    author='João Vedor',
+    author_email='fc56311@alunos.fc.ul.pt',
+    url='https://github.com/lasigeBioTM/XMR4EL',
+    python_requires=">=3.12, <3.13",
+)
