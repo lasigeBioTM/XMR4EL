14ca62602c36a6fb063b26287b93393985d621dc
Joao <fc56311@alunos.fc.ul.pt>
2025-06-09 13:47:06 +0100

    .

--- xmr4el/ranker/candidate_retrieval.py
+++ xmr4el/ranker/candidate_retrieval.py
@@ -2,8 +2,6 @@ import faiss
 
 import numpy as np
 
-from scipy.sparse import csr_matrix
-
 
 class CandidateRetrieval():
     
@@ -15,36 +13,6 @@ class CandidateRetrieval():
         self.M = M
         self.efConstruction = efConstruction
         self.efSearch = efSearch
-    
-    @staticmethod
-    def convert_predictions_into_csr(predictions, num_labels=None):
-        """
-        Converts prediction results into sparse matrix format.
-        
-        Args:
-            data: List of predictions in format [(label_index, score), ...]
-            num_labels: Total number of possible labels
-            
-        Returns:
-            csr_matrix: Sparse matrix of predictions
-        """
-        
-        rows, cols, vals = [], [], []
-        
-        for row_idx, instance in enumerate(predictions):
-            col, score = instance
-            for idx, _ in enumerate(range(len(score))):
-                rows.append(row_idx)
-                cols.append(col[idx])
-                vals.append(np.float32(score[idx]))
-
-        if num_labels is None:
-            num_labels = max(cols) + 1  # infer if not provided
-        
-        return csr_matrix((vals, (rows, cols)), 
-                          shape=(len(predictions), num_labels), 
-                          dtype=np.float32)
-    
     
     def retrival(self, conc_input, conc_emb, candidates=100):
         """
