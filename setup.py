29ec73c7f7aa43c0d5fc6137a6d7c56ca02a0d7d
Joao <fc56311@alunos.fc.ul.pt>
2025-08-04 16:12:35 +0100

    Added an pyproject.tmol for the git balanced kmeans

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
 from setuptools import setup, find_packages
 
-
-def parse_requirements(filename):
-    """Read the requirements file and return a list of dependencies"""
-    with open(filename, 'r') as file:
-        return [line.strip() for line in file.readlines() if line.strip() and not line.startswith('#')]
-
-setup(
-    name='xmr4el',
-    version='0.2',
-    packages=find_packages(),
-    install_requires=parse_requirements('requirements.txt'),
-    description='eXtreme Multi-Label Ranking for Entity Linking',
-    author='João Vedor',
-    author_email='fc56311@alunos.fc.ul.pt',
-    url='https://github.com/lasigeBioTM/XMR4EL',
-    python_requires=">=3.12"
-)
+setup()
