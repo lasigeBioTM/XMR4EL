99f086ff5586b76cfcf99401c4fd80533edf135e
Joao <fc56311@alunos.fc.ul.pt>
2025-11-07 16:19:17 +0000

    .

--- test/xmr4el/test_train_pipeline.py
+++ test/xmr4el/test_train_pipeline.py
@@ -129,7 +129,7 @@ def main():
     max_leaf_size = 200
     cut_half_cluster=True
     ranker_every_layer=True
-    depth = 2
+    depth = 4
 
     xmodel = XModel(vectorizer_config=vectorizer_config,
                     transformer_config=transformer_config,
